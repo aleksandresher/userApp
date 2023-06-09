9665bcf37de42b5fff7b4c4d9582bfeba6b1768c
aleksandresher <89658328+aleksandresher@users.noreply.github.com>
2023-06-09 20:59:01 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "production") disableReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-app">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
