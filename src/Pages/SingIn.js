5a45ade1d3e205071f993f51ac498c74bc702978
aleksandresher <89658328+aleksandresher@users.noreply.github.com>
2023-06-09 22:14:18 +0400

    Update SingIn.js

--- src/Pages/SingIn.js
+++ src/Pages/SingIn.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { keyframes } from "styled-components";
 
 function SingIn({ LoginHandler }) {
   const navigate = useNavigate();
@@ -225,26 +226,30 @@ const CreateAccountBtn = styled.button`
   margin-top: 20px;
 `;
 
-@keyframes spinner {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
+const rotate = keyframes`
+0% {
+  transform: rotate(0deg);
 }
-const SpinnerDiv = styled.div` 
-  width: 50px;
-  height: 50px;
-  border: 10px solid #f3f3f3; /* Light grey */
-  border-top: 10px solid #383636; /* Blue */
-  border-radius: 50%;
-  animation: spinner 1.5s linear infinite;
+100% {
+  transform: rotate(360deg);
+}
 `;
 
-const SpinnerContainer = styled.div` 
+const SpinnerDiv = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 5px solid #f3f3f3; /* Light grey */
+  border-top: 5px solid #1cb041; /* Blue */
+  border-radius: 50%;
+  animation: ${rotate} 1.5s linear infinite;
+`;
+
+const SpinnerContainer = styled.div`
   display: grid;
   justify-content: center;
   align-items: center;
   height: 350px;
+  position: absolute;
 `;
+
+
