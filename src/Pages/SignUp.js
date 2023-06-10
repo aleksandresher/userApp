ce49ea4d8b368aba7dd839c4ee9f4c666ae3beaa
aleksandresher <89658328+aleksandresher@users.noreply.github.com>
2023-06-10 13:17:47 +0400

    Update SignUp.js

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -243,3 +243,30 @@ const LoginBtn = styled.button`
   align-self: center;
   margin-top: 20px;
 `;
+
+const rotate = keyframes`
+0% {
+  transform: rotate(0deg);
+}
+100% {
+  transform: rotate(360deg);
+}
+`;
+
+const SpinnerDiv = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 5px solid #f3f3f3; /* Light grey */
+  border-top: 5px solid #1cb041; /* Blue */
+  border-radius: 50%;
+  animation: ${rotate} 1.5s linear infinite;
+`;
+
+const SpinnerContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  height: 350px;
+  position: absolute;
+`;
+
